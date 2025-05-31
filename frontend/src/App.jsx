8866b95a04d3df77b521ef711884cc3939306cf1
yurialcant <yurialcant@gmail.com>
2025-05-30 21:18:39 -0300

    Botão de conversa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Sobre from './components/Sobre'
-import Servicos from './components/Servicos'
-import Contato from './components/Contato'
-import Footer from './components/Footer'
+// src/App.jsx
+
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Sobre from './components/Sobre';
+import Servicos from './components/Servicos';
+import Contato from './components/Contato';
+import Footer from './components/Footer';
+import WhatsAppButton from './components/WhatsAppButton'; 
 
 function App() {
   return (
     <>
+      
       <Header />
       <Hero />
       <Sobre />
       <Servicos />
       <Contato />
       <Footer />
+      <WhatsAppButton />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
